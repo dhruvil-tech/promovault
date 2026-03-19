d20162da1bcd5a1e60c881844c716e6991940ad1
unknown <dhruvilmakwana16@gmail.com>
2026-03-19 22:55:23 +0530

    Hardcode API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://appealing-intuition-production-f5d1.up.railway.app/api',
 });
 
 // Attach JWT token to every request
